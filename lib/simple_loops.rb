3f0b37be8a450b4e53c9ace05b9a0cb80872fbf8
Darren Koomson <darrenkoomson17@gmail.com>
2020-04-23 00:30:17 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -9,40 +9,28 @@ end
 
 def loop_message_n_times(message, number)
   count = 0
-
   while count < number
     puts message
     count +=1
-
   end
-
 end
 
 def output_array(array)
-
   i = 0
-
   while i < array.length
     puts array[i]
     i += 1
   end
-
 end
 
 def return_string_array(array)
   new_arr = []
   i = 0
-  
   while i < array.length
-  new_arr << array[i].to_s
-  i +=1
-  
+    new_arr << array[i].to_s
+    i += 1
   end
-  
-  
-  
-  
-  return new_arr
+return new_arr
 end
 
 
